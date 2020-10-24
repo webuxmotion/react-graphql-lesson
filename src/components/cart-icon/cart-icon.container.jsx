47f6f996df73064ccfe60b9fd28f32cfdc12caf2
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-10-24 09:23:41 +0300

    Use graphql and compose functions

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Mutation, Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash';
 
 import CartIcon from './cart-icon.component';
 
 import { TOGGLE_CART_HIDDEN, GET_ITEM_COUNT } from '../../graphql/constants';
 
-const CartIconContainer = () => (
-  <Query query={GET_ITEM_COUNT}>
-    {
-      ({ data: { itemCount }}) => (
-        <Mutation mutation={TOGGLE_CART_HIDDEN}>
-          {
-            toggleCartHidden => (
-              <CartIcon 
-                toggleCartHidden={toggleCartHidden}
-                itemCount={itemCount}
-              />
-            )
-          }
-        </Mutation>
-      )
-    }
-  </Query>
+const CartIconContainer = ({ data: { itemCount }, toggleCartHidden }) => (
+  <CartIcon 
+    toggleCartHidden={toggleCartHidden}
+    itemCount={itemCount}
+  />
 );
 
-export default CartIconContainer;
+export default compose(
+  graphql(GET_ITEM_COUNT),
+  graphql(TOGGLE_CART_HIDDEN, { name: 'toggleCartHidden' }),
+)(CartIconContainer);
